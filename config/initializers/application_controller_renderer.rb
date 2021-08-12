4ef6d57c29f257b082387c21ad696ef00ae57c8c
末盛里彩 <suemorilisa@suemorisatoirodorinoMacBook-Pro.local>
2021-08-12 22:32:20 +0900

    [Finish]2-3~10 Rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2dbe16acc97fd603694585376cf89e5260d0e8276f24c2fc7aa00c3e007e59270eec162a662b13a4c7c1b10afc3032b8a2cd9054c6a635e4fc85b177905dc1f2'
---
+++ db/migrate/20210809091725_rename_bod_column_to_lists.rb
@@ -0,0 +1,5 @@
+class RenameBodColumnToLists < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :lists, :bod, :body
+  end
+end
